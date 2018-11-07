3814a45e68e7a85454a2b9454ea0f18dad0eb915
satyajit <satyajit015@gmail.com>
2018-11-07 06:11:47 +0530

    checking

---
+++ src/main/java/ocotopus/org/mvnsampleapplication/App.java
@@ -0,0 +1,13 @@
+package ocotopus.org.mvnsampleapplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
